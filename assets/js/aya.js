fdbbd8ad8ef5b3e1dd61a228dee4e206b0ac4b1f
aya uezato <a.u.91leo@gmail.com>
2019-05-30 10:53:35 +0900

    コミットする

--- assets/js/aya.js
+++ assets/js/aya.js
@@ -1,5 +1,7 @@
 // import { DH_UNABLE_TO_CHECK_GENERATOR } from "constants";
 
+let position0 = 
+
 $(function() {
     $('.fa-baseball-ball').on('click', function() {
         console.log("AAA");
@@ -7,7 +9,10 @@ $(function() {
             duration: 1000,
             step: function (now) {
                 // console.log('7');
-                $(this).css({ transform: 'rotate(' + now * 90 + 'deg)'})
+                $(this).css({ 
+                    transform: 'rotate(' + now * 90 + 'deg)'
+                })
+
             }
             
         });
@@ -22,8 +27,11 @@ $(function() {
         $('#over').append($('<div class="lightbox">'));
 
         let icon = $(this).find('img').clone();
+        let text = $(this).find('p').clone();
+            
 
         $('.lightbox').append(icon);
+        $('p').append(text);
 
         $('#over').on('click', function() {
             $('#over').fadeOut(function() {
@@ -32,21 +40,22 @@ $(function() {
         })
     })
 
+    
+
     $('.akirasan').on('click', function() {
         console.log('AAA');
         $('.akirasan').animate({
             'top': '-100px',
             'left': '270px',
             
-        })
+        },5000)
         
     })
     $('.akirasan').on('click',function() {
         $('.akirasan').animate({
             'top': '-190px',
-            'left': '-5px',
-            
-        })
+            'left': '-5px',  
+        },5000)
 
     
     })
@@ -56,7 +65,7 @@ $(function() {
             'top': '-100px',
             'left': '-260px',
             
-        })
+        },5000)
 
     
     })
@@ -66,7 +75,7 @@ $(function() {
             'top': '0px',
             'left': '0px',
             
-        })
+        },5000)
 
     
     })
